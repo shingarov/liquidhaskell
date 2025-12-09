66539af762ac2c33fa8e8a73914fd57fc547701e
Boris Shingarov <boris@shingarov.com>
2025-12-09 16:20:12 -0500

    Add simplified icfp15/Composition??.hs tests

---
+++ tests/benchmarks/icfp15/pos/Composition01.hs
@@ -0,0 +1,30 @@
+module Composition where
+
+{-@
+cmp :: forall < p :: b -> c -> Bool
+              , q :: a -> b -> Bool
+              , r :: a -> c -> Bool
+              >.
+       {x::a, w::b<q x> |- c<p w> <: c<r x>}
+       f:(y:b -> c<p y>)
+    -> g:(z:a -> b<q z>)
+    -> x:a -> c<r x>
+@-}
+
+cmp :: (b -> c)
+    -> (a -> b)
+    ->  a -> c
+
+cmp f g x = f (g x)
+
+
+
+{-@ incr :: x:Int -> {v:Int | v == x + 1} @-}
+incr :: Int -> Int
+incr x = x + 1
+
+
+{-@ incr2 :: x:Int -> {v:Int | v = x + 2} @-}
+incr2 :: Int -> Int
+incr2 = incr `cmp` incr
+
