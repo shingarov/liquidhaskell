c8d546f92be6a324e5e5b52ce2cb5221be4d3925
Boris Shingarov <boris@shingarov.com>
2026-02-28 13:12:48 -0500

    Add pos and neg Composition00mono

---
+++ tests/benchmarks/icfp15/pos/Composition00mono.hs
@@ -0,0 +1,18 @@
+module Composition where
+
+{-@
+cmp :: forall < p :: Int -> Int -> Bool
+              , q :: Int -> Int -> Bool
+              , r :: Int -> Int -> Bool
+              >.
+       {x::Int, w::Int<q x> |- Int<p w> <: Int<r x>}
+       f:(y:Int -> Int<p y>)
+    -> g:(z:Int -> Int<q z>)
+    -> x:Int -> Int<r x>
+@-}
+
+cmp :: (Int -> Int)
+    -> (Int -> Int)
+    ->  Int -> Int
+
+cmp f g x = f (g x)
